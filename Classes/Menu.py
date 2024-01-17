baa9a9df1c6a6375fa05dce4c3ea0bbc0de6cedc
Samuel Webb <smarkwebb@gmail.com>
2024-01-17 21:30:25 +0000

    fixed duplicate CurrentCharacter.txt

--- Classes/Menu.py
+++ Classes/Menu.py
@@ -35,7 +35,7 @@ class Menu:
                     return None
 
         # Update 'CurrentCharacter.txt'
-        file = open("./Data/Levels/CurrentCharacter.txt", "w")
+        file = open("./Data/Players/CurrentCharacter.txt", "w")
         file.write(str(character))
         file.close()
 
